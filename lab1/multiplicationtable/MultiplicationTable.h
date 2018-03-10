cceed705d6bd80942841bbe5684bc88a4ed5eaf9
kwiasylw <sylwia_664@interia.pl>
2018-03-10 17:01:13 +0100

    add files

---
+++ lab1/multiplicationtable/MultiplicationTable.h
@@ -0,0 +1,13 @@
+//
+// Created by sylwia on 10.03.18.
+//
+
+#ifndef LAB1_MULTIPLICATIONTABLE_H
+#define LAB1_MULTIPLICATIONTABLE_H
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+void MultiplicationTable(int tab[][10]);
+void PrintTable(int tab[][10]);
+#endif //LAB1_MULTIPLICATIONTABLE_H
---
+++ lab1/multiplicationtable/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by sylwia on 10.03.18.
+//
+
+#include "MultiplicationTable.h"
+
+int main() {
+    int tab [10][10];
+    MultiplicationTable(tab);
+    return 0;
+}
